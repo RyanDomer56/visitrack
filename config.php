4823ae6ac9b5cc7ecce850089af29bf121ac2cba
AshkaNozuki10 <stephenmarkmirador10@gmail.com>
2025-02-18 13:35:14 +0800

    database config array

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+return [
+    'database' => [
+        'dsn' => 'mysql:host=localhost;dbname=mood_tracker;charset=utf8mb4',
+        'username' => 'root',
+        'password' => 'kingsdeath10',
+    ],
+];

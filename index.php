ed3fa2eb18e7fdf5d8fcfb0244714318f7db960a
AshkaNozuki10 <stephenmarkmirador10@gmail.com>
2025-03-12 17:36:52 +0800

    fixed homepage

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
     <section class="section1">
         <div class="desc1">
-            <img src="image/building.png" alt="Lab building" class="building">
+            <img src="/styles/img/building.png" alt="Lab building" class="building">
             <h1>A REAL TIME LOCATION TRACKER FOR QCU VISITORS</h1>
             <p>At Quezon City University, We Believe In Creating A Safe And <br> Connected Environment For Our Students,
                 Faculty, And Staff. That's <br> Why We've Developed A Cutting-Edge Location Tracking System To <br>
@@ -36,7 +36,7 @@
 
     <section class="section2">
         <div class="desc2">
-            <img src="image/city.png" alt="City drawing" class="city">
+            <img src="/styles/img/city.png" alt="City drawing" class="city">
             <h1 id="stay-aware">SECURE CAMPUS, EMPOWERED COMMUNITY</h1>
             <br>
             <h2>Stay Aware, Stay Secure</h2>
@@ -48,7 +48,7 @@
         </div>
 
         <div class="desc3">
-            <img src="image/map.png" alt="Map drawing" class="map">
+            <img src="styles/img/map.png" alt="Map drawing" class="map">
             <h1>SAFETY WITH PRIVACY-ORIENTED TRACKING </h1>
             <br>
             <p>By Leveraging the Power of Openstreetmap-Based Location Tracking, We're <br> Putting the Tools for a
@@ -63,7 +63,7 @@
 
     <section class="section3">
         <div class="desc4">
-            <img src="image/college_students.png" alt="students drawing" class="students">
+            <img src="/styles/img/college_students.png" alt="students drawing" class="students">
             <h1>UNLOCK THE FULL POTENTIAL <br>OF CAMPUS LIFE</h1>
             <br>
             <p>Experience the Freedom to Explore Your University to the Fullest,<br> Knowing That Your Location Is Being
@@ -73,7 +73,7 @@
         </div>
 
         <div class="desc5">
-            <img src="image/security.png" alt="security image" class="security">
+            <img src="styles/img/security.png" alt="security image" class="security">
             <h1>JOIN THE MOVEMENT FOR A SAFER, <br>
                 PRIVACY-FOCUSED CAMPUS</h1>
             <br>

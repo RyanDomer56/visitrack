bf283885eba37750fe4e5120351ace98f7039e84
tambayNgOrtigasAvenue <markkengiealdabon@gmail.com>
2025-04-24 23:00:19 +0800

    additional routes for web pages

--- Herd/visitrack/routes/web.php
+++ Herd/visitrack/routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', function () {
-    return view('home_view');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/login', function () { //Url
-    return view('login_view'); //File Name
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/registration', function (){
-    return view('registration_view');
-});
+require __DIR__.'/auth.php';
